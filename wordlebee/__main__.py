f45cd88cebca79dc22211007ec552d907021743d
Mrlento234 <lento.manickathan@gmail.com>
2022-01-21 15:41:38 +0100

    added flake8, black and pre-commit check

--- wordlebee/__main__.py
+++ wordlebee/__main__.py
@@ -1,48 +1,54 @@
-import re
-import numpy as np
-from rich import print
-from rich.prompt import Prompt, Confirm
 import os
 
-def filter_word(filter_func, words):
+import numpy as np
+from rich import print
+from rich.prompt import Confirm, Prompt
+
+
+def filter_word(filter_func, words) -> np.ndarray:
     return np.array(list(filter(filter_func, words)))
 
-def get_words():
-    # Get data
-    filepath = os.path.join(os.path.dirname(__file__), 'data/words_filtered.txt')
-    return np.loadtxt(filepath, dtype='str')
 
-def cli():
-    
+def get_words() -> np.ndarray:
+    # Get data
+    filepath = os.path.join(os.path.dirname(__file__), "data/words_filtered.txt")
+    return np.loadtxt(filepath, dtype="str")
+
+
+def cli() -> None:
+
     words = get_words()
-    
+
     while True:
         # Get user input to filter words
-        char, flag = Prompt.ask('Enter [bold][[red]<Letter>[/red] [green]<position>[/green]][/bold] (position: 1-5, not: -1, unknown: 0)').split(' ')
+        raw_input = Prompt.ask(
+            "Enter [bold][[red]<Letter>[/red] [green]<position>[/green]][/bold] (position: 1-5, not: -1, unknown: 0)"
+        ).split(" ")
         # Clean input
-        char = char.lower()
-        flag = int(flag)
-        if flag == -1: # Not 
+        char = raw_input[0].lower()
+        pos = int(raw_input[1])
+        if pos == -1:
             words = filter_word(lambda x: x.find(char) == -1, words)
-        elif flag == 0:
+        elif pos == 0:
             words = filter_word(lambda x: x.find(char) != -1, words)
-        elif flag >= 1 and flag <= 5:
-            words = filter_word(lambda x: x.find(char) == flag-1, words)
+        elif pos >= 1 and pos <= 5:
+            words = filter_word(lambda x: x.find(char) == pos - 1, words)
         else:
             break
         if len(words) == 1:
-            print(f'Solution: [bold][green]{words[0]}[/green][/bold] :partying_face:')
-             
+            print(f"Solution: [bold][green]{words[0]}[/green][/bold] :partying_face:")
+
             break
         elif len(words) == 0:
-            print('Failed!', ':sob:')
-            restart = Confirm.ask('Restart guess?')
+            print("Failed!", ":sob:")
+            restart = Confirm.ask("Restart guess?")
             if restart:
                 words = get_words()
             else:
                 break
-        else:            
-            print(f'Possible words:', words)
-            
+        else:
+            print("Possible words:", words)
+
+
 if __name__ == "__main__":
     cli()
