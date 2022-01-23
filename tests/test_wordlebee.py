c0b5da5d358c6672dbb65fe1512b927793d0acbd
Mrlento234 <lento.manickathan@gmail.com>
2022-01-23 18:31:53 +0100

    added test for filter_array (#7)

--- tests/test_wordlebee.py
+++ tests/test_wordlebee.py
@@ -1,5 +1,20 @@
+import numpy as np
+
 from wordlebee import __version__
+from wordlebee.__main__ import filter_array
 
 
 def test_version():
     assert __version__ == "0.2.0"
+
+
+def test_filter_array():
+    init_list = np.array(["apple", "orange", "banana", "kiwi"])
+    filtered_list = filter_array(lambda x: x.find("b") == -1, init_list)
+    assert "banana" not in filtered_list
+
+    filtered_list = filter_array(lambda x: x.find("g") == 4, init_list)
+    assert filtered_list[0] == "orange"
+
+    filtered_list = filter_array(lambda x: x.find("a") != 0, init_list)
+    assert "apple" not in filtered_list
